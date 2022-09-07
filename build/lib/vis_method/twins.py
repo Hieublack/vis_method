1dbe637c89e40d8f74d6fc0069d2ed9271a84071
DESKTOP-TRB8SVB\Admin <tranxuanhieuhust@gmail.com>
2022-09-07 22:56:27 +0700

    update new module fo fomula and get_val_profit_com_year

--- build/lib/vis_method/twins.py
+++ build/lib/vis_method/twins.py
@@ -141,12 +141,14 @@ class Twins:
         loinhuan = []
         company = []
         value = []
+        year = []
         for j in range(len(self._index_test)-2, 0, -1):
             index_max = np.argmax(result_[self._index_test[j-1]:self._index_test[j]])+self._index_test[j-1]
             loinhuan.append(PROFIT[index_max])
             company.append(COMPANY[index_max])
             value.append(result_[index_max])
-        return value, loinhuan, company
+            year.append(self.data_test['TIME'][index_max])
+        return value, loinhuan, company, year
 
     def get_variable(self):
         list_variable = [[]]*26
